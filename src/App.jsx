e89e08464a0e24d2bf99312010f6e640e8a6fd21
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-11-12 14:17:26 +0530

    Employee Dashboard Task Number card code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 
--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,10 +1,11 @@
-import React from "react";
 import Header from "../Others/Header";
+import TaskListNumbers from "../Others/TaskListNumbers";
 
 const EmployeeDashboard = () => {
   return (
     <div className="p-10 bg-[#1C1C1C] h-screen">
       <Header />
+      <TaskListNumbers />
     </div>
   );
 };
