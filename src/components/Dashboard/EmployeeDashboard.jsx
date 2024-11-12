47898478b10840ff48c7ba9ea303787856f94047
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-11-12 16:17:06 +0530

    Employee Dashboard TaskList Card code added

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,11 +1,13 @@
 import Header from "../Others/Header";
 import TaskListNumbers from "../Others/TaskListNumbers";
+import TaskList from "../TaskList/TaskList";
 
 const EmployeeDashboard = () => {
   return (
     <div className="p-10 bg-[#1C1C1C] h-screen">
       <Header />
       <TaskListNumbers />
+      <TaskList />
     </div>
   );
 };
---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,58 @@
+const TaskList = () => {
+  return (
+    <div
+      id="tasklist"
+      className="flex overflow-x-auto items-center justify-start gap-5 flex-nowrap h-[55%] w-full py-5 mt-10"
+    >
+      <div className="flex-shrink-0 h-full w-[350px] p-5 bg-red-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">10 Nov 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make EMS Project</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          consequatur ab similique quidem. Sit, tempora.
+        </p>
+      </div>
+
+      <div className="flex-shrink-0 h-full w-[350px] p-5 bg-green-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">10 Nov 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make EMS Project</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          consequatur ab similique quidem. Sit, tempora.
+        </p>
+      </div>
+
+      <div className="flex-shrink-0 h-full w-[350px] p-5 bg-blue-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">10 Nov 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make EMS Project</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          consequatur ab similique quidem. Sit, tempora.
+        </p>
+      </div>
+
+      <div className="flex-shrink-0 h-full w-[350px] p-5 bg-yellow-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">10 Nov 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make EMS Project</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          consequatur ab similique quidem. Sit, tempora.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;
