6d8e7b6d0d413ec6f670d6f2937da9e2c65877a2
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-11-13 18:43:54 +0530

    Dynamic Data for Task List Numbers card code added

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,19 @@ import { AuthContext } from "./context/AuthProvider";
 
 const App = () => {
   const [user, setUser] = useState(null);
+  const [loggedInUserData, setLoggedInUserData] = useState(null);
+
   const authData = useContext(AuthContext);
 
-  useEffect(() => {
-    if (authData) {
-      const loggedInUser = localStorage.getItem("loggedInUser");
+  // useEffect(() => {
+  //   if (authData) {
+  //     const loggedInUser = localStorage.getItem("loggedInUser");
 
-      if (loggedInUser) {
-        setUser(loggedInUser.role);
-      }
-    }
-  }, [authData]);
+  //     if (loggedInUser) {
+  //       setUser(loggedInUser.role);
+  //     }
+  //   }
+  // }, [authData]);
 
   const handleLogin = (email, password) => {
     // check whether email and password is valid or not
@@ -24,18 +26,19 @@ const App = () => {
       setUser("admin");
 
       localStorage.setItem("loggedInUser", JSON.stringify({ role: "admin" }));
-    } else if (
-      (authData,
-      authData.employees.find(
+    } else if (authData) {
+      const employee = authData.employees.find(
         (e) => email == e.email && e.password == password
-      ))
-    ) {
-      setUser("employee");
-
-      localStorage.setItem(
-        "loggedInUser",
-        JSON.stringify({ role: "employee" })
       );
+
+      if (employee) {
+        setUser("employee");
+        setLoggedInUserData(employee);
+        localStorage.setItem(
+          "loggedInUser",
+          JSON.stringify({ role: "employee" })
+        );
+      }
     } else {
       alert("Invalid Credentials!");
     }
@@ -44,7 +47,11 @@ const App = () => {
   return (
     <>
       {!user ? <Login handleLogin={handleLogin} /> : ""}
-      {user === "admin" ? <AdminDashboard /> : <EmployeeDashboard />}
+      {user === "admin" ? (
+        <AdminDashboard />
+      ) : user === "employee" ? (
+        <EmployeeDashboard data={loggedInUserData} />
+      ) : null}
     </>
   );
 };
--- src/components/Others/TaskListNumbers.jsx
+++ src/components/Others/TaskListNumbers.jsx
@@ -1,25 +1,24 @@
-
-const TaskListNumbers = () => {
+const TaskListNumbers = ({ data }) => {
   return (
     <div className="flex mt-10 screen justify-between gap-5">
-      <div className="w-[45%] py-6 px-9 bg-red-400 rounded-xl">
-        <h2 className="text-3xl font-semibold">0</h2>
-        <h3 className="text-xl font-medium">New Task</h3>
-      </div>
-
       <div className="w-[45%] py-6 px-9 bg-blue-400 rounded-xl">
-        <h2 className="text-3xl font-semibold">0</h2>
+        <h2 className="text-3xl font-semibold">{data.taskCounts.newTask}</h2>
         <h3 className="text-xl font-medium">New Task</h3>
       </div>
 
       <div className="w-[45%] py-6 px-9 bg-green-400 rounded-xl">
-        <h2 className="text-3xl font-semibold">0</h2>
-        <h3 className="text-xl font-medium">New Task</h3>
+        <h2 className="text-3xl font-semibold">{data.taskCounts.completed}</h2>
+        <h3 className="text-xl font-medium">Completed Task</h3>
       </div>
 
       <div className="w-[45%] py-6 px-9 bg-yellow-400 rounded-xl">
-        <h2 className="text-3xl font-semibold">0</h2>
-        <h3 className="text-xl font-medium">New Task</h3>
+        <h2 className="text-3xl text-black font-semibold">{data.taskCounts.active}</h2>
+        <h3 className="text-xl text-black font-medium">Accepted Task</h3>
+      </div>
+
+      <div className="w-[45%] py-6 px-9 bg-red-400 rounded-xl">
+        <h2 className="text-3xl font-semibold">{data.taskCounts.failed}</h2>
+        <h3 className="text-xl font-medium">Failed Task</h3>
       </div>
     </div>
   );
